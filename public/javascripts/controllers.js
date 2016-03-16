5ae2b195c683ffa7a4b3e1a5a150e5c8d1f07991
mmloveaa <mandychan1128@gmail.com>
2016-03-16 19:18:59 -0400

    added modal

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -15,8 +15,57 @@ app.controller('listCtrl', function ($scope, CardFactory) {
 
 
 
-
-
-
 });
+
+app.controller('cardCtrl', function ($scope, CardFactory, CategoryFactory) {
+
+	if (!$scope.newCard) $scope.newCard = {}
+
+
+	$scope.addCard = function() {
+		CardFactory.create($scope.newCard).then(function(res) {
+			// console.log('res: ', res);
+			$('#new-card-modal').modal('hide');
+			$scope.cards.push(res.data);
+			$scope.newCard = undefined;
+		}, function(err) {
+			console.error('err: ', err);
+		});
+	}
+
+	$scope.removeCard = function(card) {
+		CardFactory.remove(card)
+		.then(function(card) {
+			//success!
+			CardFactory.fetch().then(function(res) {
+				// console.log('res: ', res);
+				$scope.cards = res.data;
+			}, function(err) {
+				console.error('err: ', err);
+			});
+
+		}, function(err) {
+			console.error(err)
+		});
+	}
+
+	$scope.editCard = function(card) {
+		$scope.cardEd = angular.copy(card);
+	}
+
+	$scope.cancelEditing = function() {
+		$scope.cardEd=undefined;
+		$scope.newCard=undefined;
+	}
+
+	$scope.commitEdit = function() {
+		CardFactory.update($scope.cardEd).then(function() {
+			$scope.cards.splice($scope.cards.findIndex(e => e.id === $scope.cardEd.id), 1, angular.copy($scope.cardEd));
+			$scope.cardEd = undefined;
+
+		});
+	}
+
+})
+
 
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -8,7 +8,13 @@ app.factory('CardFactory',function($http) {
 			return $http.get('/cards');
 		},
 		create:	function(newCard) {
+			// var categoryList = http.get("/categories")
+			// 	.then
+
+
 			return $http.post('/cards',newCard);
+
+
 		},
 		remove:	function(oldCard) {
 			var url = `/cards/${oldCard.id}`;
@@ -16,6 +22,25 @@ app.factory('CardFactory',function($http) {
 		},
 		update:	function(cardEd) {
 			return $http.put(`/cards/${cardEd.id}`,cardEd);
+		}
+	}
+});
+
+
+app.factory('CategoryFactory',function($http) {
+	return {
+		fetch: 	function() {
+			return $http.get('/categories');
+		},
+		create:	function(newCategory) {
+			return $http.post('/categories',newCategory);
+		},
+		remove:	function(oldCategory) {
+			var url = `/categories/${oldCategory.id}`;
+			return $http.delete(url);		//	promise
+		},
+		update:	function(categoryEd) {
+			return $http.put(`/categories/${categoryEd.id}`,categoryEd);
 		}
 	}
 });

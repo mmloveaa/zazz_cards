1b85a2fd9b5e8a39445837a9ca174d8e121c1f18
mmloveaa <mandychan1128@gmail.com>
2016-03-16 15:14:22 -0400

    updated home.html

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', routes);
+// app.use('/', routes);
 app.use('/cards', cards);
 app.use('/categories', categories);
 
